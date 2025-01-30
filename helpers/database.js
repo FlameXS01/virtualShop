f964b0fd502e7214d91df3459b518693d093aa55
Jailer <perezjailer41@gmail.com>
2025-01-30 18:29:00 -0500

    adada

--- helpers/database.js
+++ helpers/database.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const dotenv = require("dotenv").config();
+const pg = require("pg"); 
 
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
