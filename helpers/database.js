ad6658c96936026fc6975afc8129c112d37cfd85
Jailer <perezjailer41@gmail.com>
2025-01-30 18:24:10 -0500

    dcsv

--- helpers/database.js
+++ helpers/database.js
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     port: DB_PORT,
     logging: console.log, // Solo esta línea para logging
     native: true,
+    dialectModule: pg,
     dialectOptions: { // <-- Agrega esto para SSL
         ssl: {
         require: true,
