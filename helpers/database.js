9569bdc137b6c779a18096cc6055a4e6e21e2940
Jailer <perezjailer41@gmail.com>
2025-01-31 08:22:31 -0500

    bb

--- helpers/database.js
+++ helpers/database.js
@@ -6,7 +6,7 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DIALECT = process.env.DB_DIALECT;
-const DB_HOST = process.env.DB_HOST;
+const DB_HOST = process.env.HOST;
 const DB_PORT = process.env.DB_PORT;
 
 // Conexión a la base de datos
